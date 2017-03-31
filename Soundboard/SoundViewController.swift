3d85699870b780b8eb3aec4a7cfb7b78fd05b6d7
Jake Cameron Booth <jakecameronbooth@Jakes-MacBook-Pro.local>
2017-03-31 22:12:57 +0100

    SoundViewController created, buttons/text added and linked to view controller

---
+++ Soundboard/SoundViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Jake Cameron Booth on 31/03/2017.
+//  Copyright © 2017 Jake Cameron Booth. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
